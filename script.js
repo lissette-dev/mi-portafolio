468be44e50b8d7b84553db9108c93ce6be0b29e4
Your Name <you@example.com>
2026-04-07 16:20:27 -0400

    responsive2

--- script.js
+++ script.js
@@ -43,3 +43,11 @@ button.addEventListener("click", () => {
   });
 
 });
+
+  // HAMBURGUESA
+const hamburger = document.getElementById("hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
